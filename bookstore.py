54b9af9ba086bd35565a7147926d8d14ef49589e
luminaa <rairahual7@gmail.com>
2023-08-28 19:04:24 -0400

    Updated With New Task

--- bookstore.py
+++ bookstore.py
@@ -1,41 +1,80 @@
-#   Create an online bookstore inventory system using object-oriented programming principles. The system should allow you to manage a collection of books. Each of the books should have its own attributes e.g. title, author, genre, price, and quantity available.​
+"""
+Create an online bookstore inventory system using object-oriented programming.
+The system should allow you to manage a collection of books. 
+Each of the books should have its own attributes e.g. title, author, genre, price, and quantity available
+"""
 
-class BookStore:
+class Book:
+    def __init__(self, title, author, genre, price, quantity):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.price = price
+        self.quantity = quantity
 
-    def __init__(self,books):
-        if books:
-            self.books = books
-        else:
-            self.books = {}
-    
-    def add_book(self,title, author, genre, price, quantity):
-        self.books[title] = {'author':author,'genre':genre,'price':price,'quantity':quantity}
-    
-    def sell_book(self,title,quantity):
-        if title in self.books:
-            if self.books[title]['quantity'] >= quantity:
-                self.books[title]['quantity'] -= quantity
-                return True
-            else:
-                return False
+    def purchase(self, number):
+        if self.quantity >= number:
+            self.quantity -= number
+            return True
         else:
             return False
 
-    def price_of_transaction(self,title,quantity):
-        print("\nTransaction Details:")
-        if title in self.books and self.sell_book(title,quantity):
-            print(f"Sold {str(quantity)} {title} for ${self.books[title]['price'] * quantity}")
-        else:
-            print("not enough books in stock")
-        
-    def count_books(self):
-        print("\nBook Inventory:")
-        for book in self.books:
-            print(book,self.books[book]['quantity'])
+class Customer:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.purchased_books = {}
 
-rahuals_bookstore = BookStore({"Book A":{'author':'Rahul','genre':'Fantasy','price':10,'quantity':10}, "Book B":{'author':'Aaryan','genre':'Fantasy','price':12,'quantity':20}})
-rahuals_bookstore.add_book('Book C','Niya','Non-Fiction',10,15)
-rahuals_bookstore.count_books()
-rahuals_bookstore.price_of_transaction('Book C',2)
-rahuals_bookstore.count_books()
-    
+    def purchase_book(self, book, number):
+        if book.purchase(number):
+            self.purchased_books[book.title] = self.purchased_books.get(book.title, 0) + number
+            print(f"\nThank you, {self.first_name}! You have purchased {number} of '{book.title}'.")
+        else:
+            print(f"\nSorry, only {book.quantity} of '{book.title}' is available.")
+
+    def show_purchased_books(self):
+        if not self.purchased_books:
+            print(f"\n{self.first_name} hasn't purchased any books yet.")
+            return
+        print(f"\nBooks purchased by {self.first_name} {self.last_name}:")
+        for book, quantity in self.purchased_books.items():
+            print(f"- {quantity}x {book}")
+
+
+class BookStore:
+    def __init__(self):
+        self.books = {}
+
+    def add_book(self, book):
+        self.books[book.title] = book
+
+    def list_books(self):
+        print("\nBooks Available:")
+        for title, book in self.books.items():
+            print(f"{title} by {book.author},  Genre: {book.genre},  Price: ${book.price},  Available: {book.quantity}")
+
+    def search_by_title(self, title):
+        found_books = [book for book_title, book in self.books.items() if title.lower() in book_title.lower()]
+        if not found_books:
+            print(f"\nNo books found with title containing '{title}'.")
+            return
+        print("\nBooks Found:")
+        for book in found_books:
+            print(book.title)
+
+
+rahuals_bookstore = BookStore()
+rahuals_bookstore.add_book(Book("Book A", "Rahul", "Fantasy", 10, 10))
+rahuals_bookstore.add_book(Book("Book B", "Aaryan", "Fantasy", 12, 20))
+rahuals_bookstore.add_book(Book("Book C", "Niya", "Non-Fiction", 10, 15))
+rahuals_bookstore.list_books()
+rahuals_bookstore.search_by_title("Book")
+
+araj = Customer("Araj", "Shah", "araj.shah@gmail.com")
+araj.purchase_book(rahuals_bookstore.books["Book A"], 2)
+araj.purchase_book(rahuals_bookstore.books["Book B"], 3)
+araj.purchase_book(rahuals_bookstore.books["Book C"], 4)
+araj.show_purchased_books()
+
+rahuals_bookstore.list_books()
